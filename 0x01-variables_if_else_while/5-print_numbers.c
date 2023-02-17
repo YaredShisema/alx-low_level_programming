de54316ed8e381db1b72fbaff53d785fc48351c8
YaredShisema <yaredshi3@gmail.com>
2023-02-17 11:03:58 -0800

    if else and while statments

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+/**
+ * main - The entry point of my program
+ *
+ * Description: The program prints all single digit numbers
+ * of base 10 separated by , . followed by a space.
+ *
+ * Return: Return O if there is no error in main program
+ */
 int main(void)
 {
 	int n = 0;
